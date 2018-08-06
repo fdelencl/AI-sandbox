83c15f0d2d9b6b2c2c7607b77a66a4301bac458c
fdelencl <f.delenclos@gmail.com>
2018-08-06 16:08:32 +0000

    I'm finally ready to code

--- gradius/AI.py
+++ gradius/AI.py
@@ -1,9 +1,5 @@
 import retro
 
-
-for game in retro.data.list_games():
-	print(game, retro.data.list_states(game))
-
 env = retro.make(game='Gradius-Nes', state='Level1', record='.')
 env.reset()
 done = False
@@ -11,13 +7,8 @@ done = False
 # [??, B, Select, Start, up, down, left, right, A]
 
 while done == False:
-	actions = [0, 0, 0, 0, 0, 0, 0, 0, 1]
-	print('actions', actions)
-	_obs, _rew, done, _info = env.step(actions)
-	print('_obs', _obs.shape)
-	print('_rew', _rew)
-	print('done', done)
-	print('_info', _info)
-	env.render()
-
+        actions = [0, 0, 0, 0, 0, 0, 0, 0, 1]
+        _obs, _rew, done, _info = env.step(actions)
+        env.render()
+        
 print('what the fuck')
